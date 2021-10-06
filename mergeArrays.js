d08509b4d156d3bbb984a77d76de68987d2402e8
Andrew Schaus <andrewschaus3@gmail.com>
2021-10-06 12:44:20 -0400

    Made a second method for doing the same thing in place

--- mergeArrays.js
+++ mergeArrays.js
@@ -3,6 +3,7 @@ var one = [1,2,6,7];
 var two = [3,4,5,9];
 
 console.log(mergeArrays(one,two).toString());
+console.log(mergeArraysTwo(one,two).toString());
 
 
 function mergeArrays(arr1, arr2)
@@ -35,5 +36,28 @@ function mergeArrays(arr1, arr2)
         }
     }
     return answer;
+}
+
+function mergeArraysTwo(arr1, arr2)
+{
+    var length = arr1.length + arr2.length;
+    var j = 0;
+
+    for(var i = 0; i < length; i++)
+    {
+        if(arr2[j] < arr1[i])
+        {
+            arr1.splice(i,0,arr2[j]);
+            j++;
+        }
+
+        if(i >= arr1.length && j != arr2.length)
+        {
+            arr1.push(arr2[j]);
+            j++;
+        }
+
+    }
+    return arr1;
 }
 
